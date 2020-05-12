0b6c3ef06267d5248ba13ee2b41eb519b52c83a7
MinjeongSeo <mathmjseo@khu.ac.kr>
2020-05-13 01:35:00 +0900

    Update route

--- server/routes/dialogflow.js
+++ server/routes/dialogflow.js
@@ -1,2 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const dialogflow = require('dialogflow');
+
+const config = require('../config/keys');
+
+const projectId = config.googleProjectID
+const sessionId = config.dialogFlowSessionID
+const languageCode = config.dialogFlowSessionLanguageCode
+
+// Create a new session
+const sessionClient = new dialogflow.SessionsClient();
+const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+//Text Query Route
+
+// Event Query Route
+
+
+module.exports = router;
